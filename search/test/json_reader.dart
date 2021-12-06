dc0e3c96cda29f628f61d52a90f259d1c40f1949
chelvyn20 <chelvyn20@gmail.com>
2021-12-07 05:16:44 +0700

    commit 8th change

--- search/test/json_reader.dart
+++ search/test/json_reader.dart
@@ -6,7 +6,8 @@ String readJson(String name) {
     dir = dir.replaceAll('/test', '');
   }
   if (dir.endsWith('search')) {
-    return File('$dir/test/$name').readAsStringSync();
+    dir = dir.replaceAll('search', '');
+    return File('${dir}test/$name').readAsStringSync();
   }
-  return File('$dir/search/test/$name').readAsStringSync();
+  return File('$dir/test/$name').readAsStringSync();
 }
